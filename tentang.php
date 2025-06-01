a49b0c3d9fcbc572da89a5805db6d57c3fcf495e
Migdad Akhyar  M Isam <migdadakhyar21@gmail.com>
2025-06-01 16:21:35 +0700

    Add files via upload

---
+++ tentang.php
@@ -0,0 +1,80 @@
+<html>
+  <head>
+    <title>F-SHOP</title>
+  </head>
+  <body bgcolor=white>
+    <!-- Header -->
+    <div
+      style="
+        display: flex;
+        border-radius: 1px;
+        border: 0px lightgray solid;
+        padding: 20px;
+        width: 1879px;
+        height: 100px;
+        margin-left: -9px;
+        margin-bottom: 20px;
+        margin-top: -7px;
+        background-image: url('bg header.jpg');
+        background-size: cover;
+        background-position: center;
+      ">
+      <div style="margin-left:600px; margin-top: 10px; text-align: center; color:rgb(255, 0, 0); -webkit-text-stroke: px red; font-size: 25px; font-family: 'Arial', sans-serif;">
+        SELAMAT DATANG...SELAMAT DATANG...DI FONTAINE SHOP
+      </div>
+      <div style="margin-left: -500; margin-top: 42px; text-align: center; color: red; font-size: 20; font-family: 'Arial', sans-serif;">
+        "Kami Layani Sampai Dompet Kering"
+      </div>
+    </div>
+    
+    <!-- Menu Navigasi -->
+    <div style="display: center; align: center; margin-bottom: 1px;">
+      <center><a href="home.php">Home</a></center>
+    </div>
+    <!-- Sidebar dan Konten -->
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <!-- Baris Menu -->
+      <tr>
+        <!-- Sidebar -->
+         <br><br><br>
+        <td width="100" bgcolor="white" valign="top">
+          <div align="center">
+            <p>
+              <img src="poto.jpg" alt="foto" width="320" height="320">
+            </p>
+          </div>
+        </td>
+        <!-- Konten Utama -->
+        <td width="500%" bgcolor="#ffffff" valign="top" style="padding: 20px;">
+          <!-- Tabel Makanan -->
+          <table>
+           <p style="color: Black; font-size: 30; font-family: 'Arial', sans-serif;"> About Me </p>
+           <p style=" margin-top: -30px; color: Black; font-size: 18; font-family: 'Arial', sans-serif;"> Halo nama saya Migdad Akhyar M. Isam, seorang mahasiswa dari UNIKOM</p>
+           <p style="color: Black; font-size: 30; font-family: 'Arial', sans-serif;"> About The Company </p>
+           <p style="margin-top: -30px; color: Black; font-size: 18; font-family: 'Arial', sans-serif;"> Fontaine terinspirasi dari kata “fountain” yang berarti mata air, membawa filosofi kesegaran alami langsung dari sumbernya. Kami menyajikan makanan laut 
+               berkualitas tinggi yang dipilih dengan cermat dari lautan terbaik. Mulai dari ikan segar, udang manis, kerang pilihan, hingga olahan seafood siap saji—setiap produk kami adalah hasil perpaduan antara cita rasa laut dan sentuhan elegan.
+               Di Fontaine, kami percaya bahwa makanan laut bukan hanya tentang rasa, tapi juga pengalaman. Kami hadir untuk memenuhi selera para penikmat seafood sejati yang 
+               mencari kualitas, kesegaran, dan keaslian dalam setiap suapan.</p>
+          </table>
+ 
+        </td> 
+      </tr>
+    </table>
+
+    <!-- Footer -->
+     <br><br><br><br><br><br><br>
+    <div style= 
+    "text-align: center; 
+     border-radius: 1px;
+     border: 10px #333;
+     padding: 20px;
+     width: 1879px;
+     height: 70px;
+     margin-left: -9px;
+     margin-top: 150px;
+     background-color: #333; 
+     color: white; ">
+      <p> 2023 Fontaine Shop. All rights reserved.</p>
+    </div>
+  </body>
+</html>
